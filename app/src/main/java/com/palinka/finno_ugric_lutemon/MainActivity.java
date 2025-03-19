de518e59bdc1c88d62fb0f4b589e6e83b092139d
remeseeli <remeseeli@gmail.com>
2025-03-19 13:31:20 +0200

    publish to github

--- app/src/main/java/com/palinka/finno_ugric_lutemon/MainActivity.java
+++ app/src/main/java/com/palinka/finno_ugric_lutemon/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     // LETS F---ING GO GUYS (CENSORED DUE TO INAPPROPRIATE CONTENT)
+    // Testing comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
