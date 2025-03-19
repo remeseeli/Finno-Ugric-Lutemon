249f9ec8e069d13231791f571f1da1307aaa99a9
remeseeli <remeseeli@gmail.com>
2025-03-19 13:30:02 +0200

    first commit

---
+++ app/src/main/java/com/palinka/finno_ugric_lutemon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.palinka.finno_ugric_lutemon;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    // LETS F---ING GO GUYS (CENSORED DUE TO INAPPROPRIATE CONTENT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
