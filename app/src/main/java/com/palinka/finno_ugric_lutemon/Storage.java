340b1f83a630d85403b03b0e8de011f194b059d9
Bator Lang <Bator.Lang@student.lut.fi>
2025-04-01 20:38:41 +0300

    Have the add element method

--- app/src/main/java/com/palinka/finno_ugric_lutemon/Storage.java
+++ app/src/main/java/com/palinka/finno_ugric_lutemon/Storage.java
@@ -4,4 +4,12 @@ import java.util.HashMap;
 
 public class Storage {
     private HashMap<Integer, Lutemon> lutemonStorage = new HashMap<>();
+    private int nextId = 0;
+
+
+    public int addLutemon(Lutemon lutemon) {
+        int id = nextId++;
+        lutemonStorage.put(id, lutemon);
+        return id; //Return the ID number used for incrementation
+    }
 }
