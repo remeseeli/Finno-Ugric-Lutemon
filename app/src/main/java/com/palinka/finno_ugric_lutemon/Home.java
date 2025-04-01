5aea1119790b2c7389ed7664c18728a45c3958d3
Márton Zsombor Magyar <MArton.Magyar@student.lut.fi>
2025-04-01 15:35:28 +0300

    utalom oket

---
+++ app/src/main/java/com/palinka/finno_ugric_lutemon/Home.java
@@ -0,0 +1,11 @@
+package com.palinka.finno_ugric_lutemon;
+
+public class Home {
+
+    public Lutemon createLutemon(Lutemon lutemon){
+
+        System.out.println("Name the lutemon");
+        String lutemonName =;
+
+    }
+}
