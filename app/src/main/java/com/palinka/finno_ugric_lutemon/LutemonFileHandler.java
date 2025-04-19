b2314e1847fe01ea7b72d425dd26b02dc5b2e8e9
Bator Lang <Bator.Lang@student.lut.fi>
2025-04-19 16:13:00 +0300

    The majority of the UI is pretty good now.

--- app/src/main/java/com/palinka/finno_ugric_lutemon/LutemonFileHandler.java
+++ app/src/main/java/com/palinka/finno_ugric_lutemon/LutemonFileHandler.java
@@ -7,13 +7,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
+
 
 import java.io.*;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 
 
 public class LutemonFileHandler implements FileHandler {
